7e295df8463022c9c4da40c77b55c93b43001e7e
EmiLlam <120277587+DaikiSinji11@users.noreply.github.com>
2025-09-29 10:11:24 -0600

    Create funciones.js

---
+++ semana6/ejemplos/funciones.js
@@ -0,0 +1,11 @@
+// Arrow Function
+const suma = (a, b) => a + b;
+
+// Spread Operator
+const frutas = ["manzana", "platano"];
+const nuevasFrutas = [...frutas, "uva"];
+
+// Array Methods
+const numeros = [1,2,3,4,5];
+const pares = numeros.filter(n => n % 2 === 0);
+console.log(pares); // [2,4]
